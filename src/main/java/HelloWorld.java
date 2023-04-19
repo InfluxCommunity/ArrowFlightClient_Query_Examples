a680901427df7c626627e44eec0f68480b19d25e
Anais Dotis-Georgiou <anais@Anaiss-MacBook-Pro.local>
2023-04-18 19:02:18 -0500

    just need to print data

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,10 @@
 package com.example.helloworld;
 
 // Import Apache Arrow Flight SQL classes
+import io.grpc.CallOptions;
 import org.apache.arrow.flight.*;
+import org.apache.arrow.flight.auth2.BearerCredentialWriter;
+import org.apache.arrow.flight.grpc.CredentialCallOption;
 import org.apache.arrow.flight.sql.FlightSqlClient;
 import org.apache.arrow.memory.BufferAllocator;
 import org.apache.arrow.memory.RootAllocator;
@@ -14,20 +17,24 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         String host = "us-east-1-1.aws.cloud2.influxdata.com";
-        String query = "SELECT * FROM \"airSensors\"";
-        Location location = Location.forGrpcInsecure(host, 443).forGrpcTls(host, 0);
+        String query = "SELECT * from \"airSensors\"";
+        //String query = "SELECT 1";
+        Location location = Location.forGrpcTls(host, 443);
 
         CallHeaders headers = new FlightCallHeaders();
-        headers.insert("token", "pnsXlP9U_q-AQC6MXK-HS6jeatkT5dBpkt8BGjHdNEn-IhEk1Zysaj9h2l3LCsF6QtQZ7qG0rdU9bxK_YiCuzQ==");
+
         headers.insert("bucket-name", "anais-iox");
         HeaderCallOption headerOption = new HeaderCallOption(headers);
+        CredentialCallOption auth = new CredentialCallOption(new BearerCredentialWriter("9cwj30lxAZWtumTys3TBJ3YyPPeIVR1SNPec_nCMYAM8NK_K5cqLxPUZSGKJg4aHIo35tUVY--6fjv9shWTAiw=="));
         BufferAllocator allocator = new RootAllocator(Long.MAX_VALUE);
        
-        FlightClient client = FlightClient.builder(allocator, location).build(); 
+        FlightClient client = FlightClient.builder(allocator, location)
+                .verifyServer(false)
+                .build();
         System.out.println("client" + client);
         FlightSqlClient sqlClient = new FlightSqlClient(client);
         System.out.println( "sqlClient: " + sqlClient);
-        FlightInfo flightInfo = sqlClient.execute(query, headerOption);
+        FlightInfo flightInfo = sqlClient.execute(query, headerOption, auth);
         // final FlightStream stream = sqlClient.getStream(flightInfo.getEndpoints().get(0).getTicket(), headerOption);
         // while (stream.next()) {
         //   try { final VectorSchemaRoot root = stream.getRoot(); 
@@ -40,3 +47,11 @@ public class HelloWorld {
         // stream.close();
     }
 }
+
+// <dependency>
+// <groupId>io.grpc</groupId>
+// <artifactId>grpc-netty-shaded</artifactId>
+// <version>1.41.0</version>
+// </dependency>
+
+

47c44de9fecc5a56c7c0aaa6fe35fa3439eda9ea
Anais Dotis-Georgiou <anais@Anaiss-MacBook-Pro.local>
2023-04-18 19:40:54 -0500

    works

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,4 @@
 package com.example.helloworld;
-
 // Import Apache Arrow Flight SQL classes
 import io.grpc.CallOptions;
 import org.apache.arrow.flight.*;
@@ -12,51 +11,58 @@ import org.apache.arrow.vector.VectorSchemaRoot;
 import io.grpc.Metadata;
 import java.net.URI;
 
-
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        String host = "us-east-1-1.aws.cloud2.influxdata.com";
-        String query = "SELECT * from \"airSensors\"";
-        //String query = "SELECT 1";
+        String host = "<host without https:// i.e. us-east-1-1.aws.cloud2.influxdata.com>";
+        String query = "SELECT 1";
         Location location = Location.forGrpcTls(host, 443);
 
-        CallHeaders headers = new FlightCallHeaders();
-
-        headers.insert("bucket-name", "anais-iox");
-        // headers.insert("database", "anais-iox");
-        HeaderCallOption headerOption = new HeaderCallOption(headers);
-        CredentialCallOption auth = new CredentialCallOption(new BearerCredentialWriter(""));
+        CredentialCallOption auth = new CredentialCallOption(new BearerCredentialWriter("<your token>"));
         BufferAllocator allocator = new RootAllocator(Long.MAX_VALUE);
-       
+
+        // We're creating an interceptor here to inject the database header on every
+        // request, unconditionally.
+        FlightClientMiddleware.Factory f = info -> new FlightClientMiddleware() {
+            @Override
+            public void onBeforeSendingHeaders(CallHeaders outgoingHeaders) {
+                outgoingHeaders.insert("database", "<your bucket>");
+            }
+
+            @Override
+            public void onHeadersReceived(CallHeaders incomingHeaders) {
+
+            }
+
+            @Override
+            public void onCallCompleted(CallStatus status) {
+
+            }
+        };
+
         FlightClient client = FlightClient.builder(allocator, location)
-                .verifyServer(false)
+                .intercept(f)
                 .build();
         System.out.println("client" + client);
         FlightSqlClient sqlClient = new FlightSqlClient(client);
-        System.out.println( "sqlClient: " + sqlClient);
-        FlightInfo flightInfo = sqlClient.execute(query, headerOption, auth);
-        FlightStream stream = sqlClient.getStream(flightInfo.getEndpoints().get(0).getTicket(), headerOption, auth);
-        System.out.println( "stream: " + stream);
-        VectorSchemaRoot root = stream.getRoot();
-        System.out.println( "root: " + root);
-        System.out.println(root.contentToTSVString());
-        // while (stream.next()) {
-        //   try { final VectorSchemaRoot root = stream.getRoot(); 
-        //     System.out.println(root.contentToTSVString());
-        //   }  catch (Exception e) {
-        // // handle the exception here, e.g. print error message
-        // System.out.println("Error executing FlightSqlClient: " + e.getMessage());
-        //   }
-        // } 
-        // stream.close();
+        System.out.println("sqlClient: " + sqlClient);
+        FlightInfo flightInfo = sqlClient.execute(query, auth);
+
+        final FlightStream stream = sqlClient.getStream(flightInfo.getEndpoints().get(0).getTicket(), auth);
+        while (stream.next()) {
+            try {
+                final VectorSchemaRoot root = stream.getRoot();
+                System.out.println(root.contentToTSVString());
+            } catch (Exception e) {
+                // handle the exception here, e.g. print error message
+                System.out.println("Error executing FlightSqlClient: " + e.getMessage());
+            }
+        }
+        try {
+            stream.close();
+        } catch (Exception e) {
+            // handle the exception here, e.g. print error message
+            System.out.println("Error closing stream: " + e.getMessage());
+        }
     }
 }
-
-// <dependency>
-// <groupId>io.grpc</groupId>
-// <artifactId>grpc-netty-shaded</artifactId>
-// <version>1.41.0</version>
-// </dependency>
-
-

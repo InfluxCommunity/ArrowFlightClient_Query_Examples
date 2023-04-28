0a950e98bca85ed026f954f3a91f9393ef63ef3d
Anais Dotis-Georgiou <anais@MacBook-Pro.local>
2023-04-28 14:40:55 -0500

    added go example and reorganized

---
+++ Go/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"context"
+	"crypto/x509"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/apache/arrow/go/v12/arrow/flight/flightsql"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
+)
+
+func main() {
+	fmt.Println("An example using Go Flight SQL Client to query InfluxDB")
+
+	if err := dbQuery(context.Background()); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func dbQuery(ctx context.Context) error {
+	url := "<host url with port 433 e.g. us-east-1-1.aws.cloud2.influxdata.com:443>"
+	token := "<your token>"
+	bucket := "<your bucket>"
+
+	// Create a gRPC transport
+	pool, err := x509.SystemCertPool()
+	if err != nil {
+		return fmt.Errorf("x509: %s", err)
+	}
+	transport := grpc.WithTransportCredentials(credentials.NewClientTLSFromCert(pool, ""))
+	opts := []grpc.DialOption{
+		transport,
+	}
+
+	// Create query client
+	client, err := flightsql.NewClient(url, nil, nil, opts...)
+	if err != nil {
+		return fmt.Errorf("flightsql: %s", err)
+	}
+
+	ctx = metadata.AppendToOutgoingContext(ctx, "authorization", "Bearer "+token)
+	ctx = metadata.AppendToOutgoingContext(ctx, "bucket-name", bucket)
+
+	// Execute query
+	query := `SELECT * FROM 'measurementName'`
+
+	info, err := client.Execute(ctx, query)
+	if err != nil {
+		return fmt.Errorf("flightsql flight info: %s", err)
+	}
+	reader, err := client.DoGet(ctx, info.Endpoint[0].Ticket)
+	if err != nil {
+		return fmt.Errorf("flightsql do get: %s", err)
+	}
+
+	// Print results as JSON
+	for reader.Next() {
+		record := reader.Record()
+		b, err := json.MarshalIndent(record, "", "  ")
+		if err != nil {
+			return err
+		}
+		fmt.Println("RECORD BATCH")
+		fmt.Println(string(b))
+
+		if err := reader.Err(); err != nil {
+			return fmt.Errorf("flightsql reader: %s", err)
+		}
+	}
+
+	return nil
+}

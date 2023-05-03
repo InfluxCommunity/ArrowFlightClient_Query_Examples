c012cb628fc1c96de17cdf138350f60464943f0f
Jason Stirnaman <stirnamanj@gmail.com>
2023-05-03 13:19:30 -0500

    fix(scripts): remove unneeded function wrappers from build scripts.
    - sh ./influxbuild.sh

--- Java/influxdb-build.sh
+++ Java/influxdb-build.sh
@@ -1,8 +1,6 @@
-function build() {
-  docker build \
-    --build-arg DATABASE_NAME="database" \
-    --build-arg DATABASE_NAME=$INFLUX_DATABASE \
-    --build-arg HOST=$INFLUX_HOST \
-    --build-arg TOKEN=$INFLUX_TOKEN \
-    -t javaflight .
-}
+docker build \
+  --build-arg DATABASE_NAME="database" \
+  --build-arg DATABASE_NAME=$INFLUX_DATABASE \
+  --build-arg HOST=$INFLUX_HOST \
+  --build-arg TOKEN=$INFLUX_TOKEN \
+  -t javaflight .

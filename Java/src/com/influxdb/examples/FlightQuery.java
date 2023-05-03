bceda45a45e48f49b0348cd5f28141ad4d5bde4b
Jason Stirnaman <stirnamanj@gmail.com>
2023-05-03 10:47:23 -0500

    fix(java-example): comment and organize:
    - Add comments from documentation.
    - Group created objects with the methods that use them.
    - Close the client.
    - Specify the ticket, for clarity.

--- Java/src/com/influxdb/examples/FlightQuery.java
+++ Java/src/com/influxdb/examples/FlightQuery.java
@@ -1,9 +1,5 @@
-package com.example.javaexample;
+package com.influxdb.examples;
 
-// Import Apache Arrow Flight SQL classes
-import io.grpc.CallOptions;
-import io.grpc.Metadata;
-import java.net.URI;
 import org.apache.arrow.flight.auth2.BearerCredentialWriter;
 import org.apache.arrow.flight.CallHeaders;
 import org.apache.arrow.flight.CallStatus;
@@ -11,18 +7,17 @@ import org.apache.arrow.flight.grpc.CredentialCallOption;
 import org.apache.arrow.flight.Location;
 import org.apache.arrow.flight.FlightClient;
 import org.apache.arrow.flight.FlightClientMiddleware;
-import org.apache.arrow.flight.FlightClientMiddleware.Factory;
 import org.apache.arrow.flight.FlightInfo;
 import org.apache.arrow.flight.FlightStream;
 import org.apache.arrow.flight.sql.FlightSqlClient;
+import org.apache.arrow.flight.Ticket;
 import org.apache.arrow.memory.BufferAllocator;
 import org.apache.arrow.memory.RootAllocator;
 import org.apache.arrow.vector.VectorSchemaRoot;
 
-public class JavaExample {
+public class FlightQuery {
 
-    /* Get environment variables */
-    public static final String DATABASE_FIELD = System.getenv("DATABASE_FIELD");
+    /* Get server credentials from environment variables */
     public static final String DATABASE_NAME = System.getenv("DATABASE_NAME");
     public static final String HOST = System.getenv("HOST");
     public static final String TOKEN = System.getenv("TOKEN");
@@ -30,18 +25,11 @@ public class JavaExample {
     public static void main(String[] args) {
         System.out.println("Query InfluxDB with the Java Flight SQL Client");
 
-        String query = "SELECT * FROM home";
-        Location location = Location.forGrpcTls(HOST, 443);
-
-        CredentialCallOption auth = new CredentialCallOption(new BearerCredentialWriter(TOKEN));
-        BufferAllocator allocator = new RootAllocator(Long.MAX_VALUE);
-
-        // We're creating an interceptor here to inject the database header on every
-        // request, unconditionally.
+        // Create an interceptor that injects header metadata (database name) in every request.
         FlightClientMiddleware.Factory f = info -> new FlightClientMiddleware() {
             @Override
             public void onBeforeSendingHeaders(CallHeaders outgoingHeaders) {
-                outgoingHeaders.insert(DATABASE_FIELD, DATABASE_NAME);
+                outgoingHeaders.insert("database", DATABASE_NAME);
             }
 
             @Override
@@ -55,29 +43,69 @@ public class JavaExample {
             }
         };
 
+        // Create a gRPC+TLS channel URI with HOST and port 443.
+        Location location = Location.forGrpcTls(HOST, 443);
+
+        // Set the allowed memory.
+        BufferAllocator allocator = new RootAllocator(Long.MAX_VALUE);
+
+        // Create a client with the allocator and gRPC channel.
         FlightClient client = FlightClient.builder(allocator, location)
                 .intercept(f)
                 .build();
         System.out.println("client" + client);
+
         FlightSqlClient sqlClient = new FlightSqlClient(client);
         System.out.println("sqlClient: " + sqlClient);
+
+        // Define the SQL query to execute.
+        String query = "SELECT * FROM home";
+        
+        /*  Construct a bearer credential using TOKEN.
+            Construct a credentials option using the bearer credential.
+        */
+        CredentialCallOption auth = new CredentialCallOption(new BearerCredentialWriter(TOKEN));
+
+        /*  Execute the query.
+            If successful, execute returns a FlightInfo object that contains metadata
+            and an endpoints list.
+            Each endpoint contains the following:
+                - A list of addresses where you can retrieve the data.
+                - A `ticket` value that identifies the data to retrieve.
+        */
         FlightInfo flightInfo = sqlClient.execute(query, auth);
 
-        final FlightStream stream = sqlClient.getStream(flightInfo.getEndpoints().get(0).getTicket(), auth);
+        // Extract the Flight ticket from the response.
+        Ticket ticket = flightInfo.getEndpoints().get(0).getTicket();
+        
+        // Pass the ticket to request the Arrow stream data from the endpoint.
+        final FlightStream stream = sqlClient.getStream(ticket, auth);
+
+        // Process all the Arrow stream data.
         while (stream.next()) {
             try {
+                // Get the current vector data from the stream.
                 final VectorSchemaRoot root = stream.getRoot();
                 System.out.println(root.contentToTSVString());
             } catch (Exception e) {
-                // handle the exception here, e.g. print error message
+                // Handle exceptions.
                 System.out.println("Error executing FlightSqlClient: " + e.getMessage());
             }
         }
         try {
+            // Close the stream and release resources.
             stream.close();
         } catch (Exception e) {
-            // handle the exception here, e.g. print error message
+            // Handle exceptions.
             System.out.println("Error closing stream: " + e.getMessage());
+        }
+
+        try {
+            // Close the client
+            sqlClient.close();
+        } catch (Exception e) {
+            // Handle exceptions.
+            System.out.println("Error closing client: " + e.getMessage());
         }
     }
 }
